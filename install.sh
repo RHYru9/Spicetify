eab77dfcff9a0dd9ce9b1d5eedf74485c7eda3e9
Rhyru9 <164749781+RHYru9@users.noreply.github.com>
2025-03-22 05:30:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,25 +2,29 @@
 
 set -e
 
-# Download URL
-theme_url="https://raw.githubusercontent.com/Comfy-Themes/Spicetify/main/Comfy"
+# Clone repository
+git clone https://github.com/RHYru9/Spicetify/ temp_spicetify
 
 # Setup directories to download to
 spice_dir="$(dirname "$(spicetify -c)")"
-theme_dir="${spice_dir}/Themes"
+theme_dir="${spice_dir}/Themes/Comfy"
 
 # Make directories if needed
-mkdir -p "${theme_dir}/Comfy"
+mkdir -p "${theme_dir}"
 
-# Download latest tagged files into correct director
-echo "Downloading Comfy..."
-curl --silent --output "${theme_dir}/Comfy/color.ini" "${theme_url}/color.ini"
-curl --silent --output "${theme_dir}/Comfy/user.css" "${theme_url}/user.css"
-curl --silent --output "${theme_dir}/Comfy/theme.js" "${theme_url}/theme.js"
-echo "Done"
+# Move necessary files
+echo "Copying Comfy theme files..."
+cp temp_spicetify/Comfy/color.ini "${theme_dir}/color.ini"
+cp temp_spicetify/Comfy/user.css "${theme_dir}/user.css"
+cp temp_spicetify/Comfy/theme.js "${theme_dir}/theme.js"
+
+# Remove temporary clone
+rm -rf temp_spicetify
+
+echo "Done!"
 
 # Apply theme
-echo "Applying theme"
+echo "Applying theme..."
 spicetify config current_theme Comfy color_scheme Comfy
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
 spicetify apply
